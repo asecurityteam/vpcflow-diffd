ab3e79f9c92fd022b2b5938391f88c4b3df6ca7f
Kevin Conway <kconway@atlassian.com>
2019-02-01 17:04:36 -0600

    Surface error if progress timeout is invalid

--- pkg/runtime.go
+++ pkg/runtime.go
@@ -93,7 +93,10 @@ func (s *Service) init() error {
 	}
 	if s.Storage == nil {
 		progressTimeoutStr := mustEnv("DIFF_PROGRESS_TIMEOUT")
-		progressTimeoutInt, _ := strconv.Atoi(progressTimeoutStr)
+		progressTimeoutInt, err := strconv.Atoi(progressTimeoutStr)
+		if err != nil {
+			return err
+		}
 		s.Storage = &storage.InProgress{
 			Bucket: mustEnv("DIFF_PROGRESS_BUCKET"),
 			Client: progressClient,
